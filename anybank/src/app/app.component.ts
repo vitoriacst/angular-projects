c2712dce9abe7a687710b55053e78d5657881127
vitoriacst <vitoria.cst.holanda@gmail.com>
2026-02-08 19:57:40 -0300

    feat: :sparkles: creating banner component

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BannerComponent } from './components/banner/banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
