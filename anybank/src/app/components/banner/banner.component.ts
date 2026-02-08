cd3f33db0ed0dec8a8eb91616f73758a773dbb74
vitoriacst <vitoria.cst.holanda@gmail.com>
2026-02-08 20:23:20 -0300

    feat: :sparkles: calling new components in banner.component

--- anybank/src/app/components/banner/banner.component.ts
+++ anybank/src/app/components/banner/banner.component.ts
@@ -1,10 +1,10 @@
-import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { AccountComponent } from './account/account.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-banner',
-  imports: [DatePipe, WelcomeComponent],
+  imports: [WelcomeComponent, AccountComponent],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
